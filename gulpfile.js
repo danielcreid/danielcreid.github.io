9620f0a3c155e2d106616f4be074e0c4cd620894
Daniel Reid <danielc.reid@gmail.com>
2014-09-29 18:46:10 -0500

    Adding Jekyll

--- gulpfile.js
+++ gulpfile.js
@@ -9,17 +9,22 @@ gulp.task('styles', function() {
         .pipe(gulp.dest('assets/build'));
 });
 
-gulp.task('copyfonts', function() {
-    gulp.src('assets/source/fonts/**/*.*')
-        .pipe(gulp.dest('assets/build/fonts'));
+gulp.task('jekyll', function () {
+    require('child_process').exec('jekyll serve --watch');
 });
 
 gulp.task('clean', function(cb) {
     del(['assets/build/**/*.*'], cb);
 });
 
+gulp.task('copyfonts', function() {
+    gulp.src('assets/source/fonts/**/*.*')
+        .pipe(gulp.dest('assets/build/fonts'));
+});
+
 gulp.task('watch', function() {
+    gulp.watch(['./_drafts/**', './_includes/**', './_layouts/**', './_posts/**', 'index.html'], ['jekyll']);
     gulp.watch('assets/source/scss/**/*.scss', ['styles']);
 });
 
-gulp.task('default', ['clean', 'copyfonts', 'styles', 'watch']);
+gulp.task('default', ['clean', 'jekyll', 'copyfonts', 'styles', 'watch']);
