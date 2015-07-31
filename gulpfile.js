994a86f1d218be2450295fd5ba68045402527e63
Daniel Reid <danielc.reid@gmail.com>
2015-07-30 20:14:16 -0500

    Minor enhancements
    - Adding SVG sprite and changing logo to SVG
    - Adding theme-color
    - Making the top body-border a bit thicker and allowing it to scroll
    with the page
    - Removing Bebas font dependency
    - Inlining styles for better performance

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,14 @@ var gulp = require('gulp'),
 
 gulp.task('styles', function() {
     gulp.src('source/styles.scss')
-        .pipe(rubySass({style: 'compressed'}))
+        .pipe(rubySass({
+            style: 'compressed',
+            'sourcemap=none': true
+        }))
         .pipe(autoprefixer({
             browsers: ['last 2 versions', 'ie >= 9']
         }))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('_includes'));
 });
 
 gulp.task('scripts', function() {
@@ -24,15 +27,10 @@ gulp.task('jekyll', function () {
     require('child_process').exec('jekyll serve');
 });
 
-gulp.task('copyfonts', function() {
-    gulp.src('source/fonts/**/*.*')
-        .pipe(gulp.dest('dist/fonts'));
-});
-
 gulp.task('watch', function() {
     gulp.watch(['_drafts/**', '_includes/**', '_layouts/**', '_posts/**', 'index.html'], ['jekyll']);
     gulp.watch('source/js/*.js', ['scripts']);
     gulp.watch('source/**/*.scss', ['styles']);
 });
 
-gulp.task('default', ['copyfonts', 'scripts', 'styles', 'jekyll', 'watch']);
+gulp.task('default', ['scripts', 'styles', 'jekyll', 'watch']);
